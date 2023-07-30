cae67fdfdd3919316f9b19bd64b67283f8ef25c5
Brandon Rubio <brubio3724@gmail.com>
2023-07-30 11:30:57 -0700

    Config verifies that environment variables are defined

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,13 +2,22 @@ import { config } from 'dotenv';
 
 config();
 
-const { CLIENT_TOKEN, CLIENT_ID, MONGO_URL } = process.env;
+const { NODE_ENV, CLIENT_TOKEN, CLIENT_ID, MONGO_URL } = process.env;
 
 if (!CLIENT_TOKEN) {
-  throw new Error('Client token didnt load');
+  throw new Error('Client token is not defined in environment variables');
+}
+
+if (!CLIENT_ID) {
+  throw new Error('Client id is not defined in environment variables');
+}
+
+if (!MONGO_URL) {
+  throw new Error('Mongodb URL is not defined in environment variables');
 }
 
 export const env: Record<string, string> = {
+  NODE_ENV,
   CLIENT_TOKEN,
   CLIENT_ID,
   MONGO_URL,
