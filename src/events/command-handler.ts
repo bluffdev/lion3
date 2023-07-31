a819e72ab6f1819a4841002e4e45193a7557e4cd
Brandon Rubio <brubio3724@gmail.com>
2023-07-30 18:52:01 -0700

    Updated command handler to check for channel permissions

--- src/events/command-handler.ts
+++ src/events/command-handler.ts
@@ -1,17 +1,38 @@
-import { AutocompleteInteraction, CacheType, CommandInteraction } from 'discord.js';
+import {
+  AutocompleteInteraction,
+  CacheType,
+  ChatInputCommandInteraction,
+  CommandInteraction,
+} from 'discord.js';
 import { Command } from '../commands/command';
 import { EventHandler } from './';
-import { findCommand } from '../utils';
+import { findCommand, Logger, reply } from '../utils';
 
 export class CommandHandler implements EventHandler {
   constructor(public commands: Command[]) {}
 
   public async process(intr: CommandInteraction | AutocompleteInteraction): Promise<void> {
     let command = findCommand(this.commands, [intr.commandName]);
+
+    const allowedChannels = command.channels;
+
+    if (allowedChannels) {
+      const channel = intr.channel.name;
+      if (!allowedChannels.includes(channel)) {
+        await reply(
+          intr as ChatInputCommandInteraction,
+          `This command is only allowed in these channels: ${allowedChannels}`,
+          true
+        );
+        return;
+      }
+    }
+
     try {
       command.execute(intr as CommandInteraction<CacheType>);
-    } catch (err) {
-      console.error('Error executing command', err);
+      Logger.info(`Executed ${command.name} command`);
+    } catch (error) {
+      console.error('Error executing command', error);
     }
   }
 }
