f4346be51089a1745fdfc142262c5c5e1a5621b8
Brandon Rubio <brubio3724@gmail.com>
2023-09-01 13:01:09 -0400

    Removed moderation stuff

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,31 +2,16 @@ import { REST } from 'discord.js';
 import { env } from '../utils';
 import { ClientService } from './client-service';
 import { GuildService } from './guild-service';
-import { ModerationService } from './moderation-service';
-import { WarningService } from './warning-service';
 import { ClassService } from './class-service';
 import { CommandService } from './command-service';
 
 import RegisterCommand from '../commands/channels/register';
 import UnRegisterCommand from '../commands/channels/unregister';
-import AnonReportCommand from '../commands/moderation/anonreport';
-import ModListCommand from '../commands/moderation/modlist';
-import ModReportCommand from '../commands/moderation/modreport';
-import PingCommand from '../commands/moderation/ping';
 
-export const clientService = new ClientService([
-  new RegisterCommand(),
-  new UnRegisterCommand(),
-  new AnonReportCommand(),
-  new ModListCommand(),
-  new ModReportCommand(),
-  new PingCommand(),
-]);
+export const clientService = new ClientService([new RegisterCommand(), new UnRegisterCommand()]);
 export const commandService = new CommandService(
   new REST({ version: '10' }).setToken(env.CLIENT_TOKEN),
   clientService
 );
 export const guildService = new GuildService(clientService);
-export const warningService = new WarningService(clientService);
-export const moderationService = new ModerationService(guildService, warningService);
 export const classService = new ClassService(guildService);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,4 @@ export * from './mongo-utils';
 export * from './logger';
 export * from './interaction-utils';
 export * from './command-utils';
-export * from './moderation-utils';
 export * from './class-utils';
